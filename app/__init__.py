0e8463e1ca31d64059eb994f38074423a10ab241
matheus-narracci <54646134+matheus-narracci@users.noreply.github.com>
2024-05-28 20:46:23 -0300

    adicionando conexao com swagger e abrindo porta para rede publica

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .config import Config
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from flask_swagger_ui import get_swaggerui_blueprint
 
 
 app = Flask(__name__)
@@ -13,9 +14,26 @@ jwt = JWTManager(app)
 
 
 
+SWAGGER_URL = '/api/docs'  
+API_URL = "/static/openapi.json" 
+
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL, 
+    API_URL,
+    config={  
+        'app_name': "Embrapa Vitibrasil API"
+    },
+
+)
+
+
 from app import models
 from app.routes import routes
 from app.auth import auth
 
+app.register_blueprint(swaggerui_blueprint)
 app.register_blueprint(routes, url_prefix='/api')
 app.register_blueprint(auth, url_prefix='/auth')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) 
